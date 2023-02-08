ec2d84520d7db33bd2d4520b481da2357afc26b7
yangdeliu <yangdeliu@sics.com>
2023-02-08 16:25:03 +0800

    feat: support uds login with data path
    Signed-off-by: yangdeliu <yangdeliu@sics.com>

--- _examples/simple/main.go
+++ _examples/simple/main.go
@@ -14,24 +14,27 @@ func getYasdbConn(dsn string) (*sql.DB, error) {
 
 func main() {
     dsn := example.GetDsn()
+    // dsn = "sys/Cod-2022@127.0.0.1:1688"
+    dsn = "/home/yangdeliu/gitlab/anchorbase/install/data/yasdb/mn-1-1"
     db, err := sql.Open("yasdb", dsn)
     if err != nil {
         fmt.Println("failed to connect yashandb, err:", err)
         return
     }
     defer db.Close()
-    rows, err := db.Query("select version from v$instance")
+    rows, err := db.Query("select name, value from v$parameter where name='RUN_LOG_FILE_PATH'")
     if err != nil {
         fmt.Println(err)
         return
     }
+    fmt.Println("select name, value from v$parameter where name='RUN_LOG_FILE_PATH'")
     for rows.Next() {
-        var version string
-        err = rows.Scan(&version)
+        var name, value string
+        err = rows.Scan(&name, &value)
         if err != nil {
             fmt.Println(err)
             return
         }
-        fmt.Println("YashanDB version:", version)
+        fmt.Println(name, "=>", value)
     }
 }
